c00b4613925eb96cd006a64405f1b21cfe15a944
zeltoddd <113621279+zeltoddd@users.noreply.github.com>
2024-01-16 04:02:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
             const speed = element.getAttribute('data-speed');
             const yPos = -window.scrollY * speed;
             element.style.transform = 'translateY(' + yPos + 'px)';
+        
+        });
+    });
+
+    const navIcons = document.querySelectorAll('#nav-icon1, #nav-icon2, #nav-icon3, #nav-icon4');
+
+    navIcons.forEach(function (icon) {
+        icon.addEventListener('click', function () {
+            icon.classList.toggle('open');
         });
     });
 })
