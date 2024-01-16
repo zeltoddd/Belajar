e1b96c9043a362e7d00aeaeee760350ecc6703ec
zeltoddd <113621279+zeltoddd@users.noreply.github.com>
2024-01-17 02:16:54 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -34,4 +34,15 @@ document.addEventListener('DOMContentLoaded', function() {
         // update previous scroll position
         prevScrollPos = currentScrollPos;
         });
-})
+        document.getElementById("toggle-navbar").addEventListener("click", () => {
+            const element = document.getElementById("nav-items");
+            if (element.classList.contains("block")) {
+              element.classList.remove("block");
+              element.classList.add("hidden");
+            } else {
+              element.classList.remove("hidden");
+              element.classList.add("block");
+            }
+    
+        });
+});
